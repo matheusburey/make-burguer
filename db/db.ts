c810bddc5604c6d5c57e4ded7dfc849af91bbae6
matheusburey <matheushburei.99@gmail.com>
2022-08-21 15:43:00 -0300

    feat: first commit

---
+++ db/db.ts
@@ -0,0 +1,37 @@
+export const status = [
+  { id: 1, type: "Solicitado" },
+  { id: 2, type: "Em produção" },
+  { id: 3, type: "Finalizado" },
+];
+
+export const ingredients = {
+  breads: [
+    { id: 1, type: "Italiano Branco" },
+    { id: 2, type: "Queijos" },
+    { id: 3, type: "Parmesão e Orégano" },
+    { id: 4, type: "Integral" },
+  ],
+  meat: [
+    { id: 1, type: "Maminha" },
+    { id: 2, type: "Alcatra" },
+    { id: 3, type: "Picanha" },
+    { id: 4, type: "Veggie Burger" },
+  ],
+  optional: [
+    { id: 1, type: "Bacon" },
+    { id: 2, type: "Cheddar" },
+    { id: 3, type: "Salame" },
+    { id: 4, type: "Tomate" },
+    { id: 5, type: "Cebola" },
+    { id: 6, type: "Pepino" },
+  ],
+};
+
+export const requests = [
+  {
+    name: "Matheus",
+    meat: "Picanha",
+    breads: "Italiano Branco",
+    optional: ["Bacon", "Cebola"],
+  },
+];
---
+++ src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/"><img src="" alt=""></RouterLink>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+</style>
