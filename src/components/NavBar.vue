16418e4565c48a1037d33cbc29e08e9be1fdf4a2
matheusburey <matheushburei.99@gmail.com>
2022-08-22 10:20:08 -0300

    feat: update router and views

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
+import { RouterLink } from "vue-router";
 </script>
 
 <template>
   <header>
     <nav>
-      <RouterLink to="/"><img src="" alt=""></RouterLink>
+      <RouterLink to="/"><img src="" alt="" /></RouterLink>
       <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
+      <RouterLink to="/pedidos">Pedidos</RouterLink>
     </nav>
   </header>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
---
+++ src/views/RequestsView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>Pedidos</h1>
+  </div>
+</template>
+
+<style scoped>
+</style>
